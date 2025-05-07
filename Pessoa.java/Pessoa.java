4ebcf37b9411f777a9944d6b361a4f45b24e5f2f
Gabriel F <78265172+G-F-D-L@users.noreply.github.com>
2025-05-07 11:15:46 -0300

    .
    consertar pessoa.java

--- Pessoa.java/Pessoa.java
+++ Pessoa.java/Pessoa.java
@@ -1,4 +1,17 @@
-public class Pessoa {
-    
+//class
+public class Pessoa{
+    //atributos
+    private String nome;
+    private int idade;
+
+    //construtor    
+    public Pessoa(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    //método
+    public void falar(){
+    System.out.println("Olá mundo");
+    }
 }
-//Achar o código da pessoa.java
