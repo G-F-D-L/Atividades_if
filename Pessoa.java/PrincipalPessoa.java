95629a6674216299b9b437d7c1ebf5e07c08078c
Gabriel F <78265172+G-F-D-L@users.noreply.github.com>
2025-05-14 11:25:10 -0300

    toString part II

--- Pessoa.java/PrincipalPessoa.java
+++ Pessoa.java/PrincipalPessoa.java
@@ -2,6 +2,9 @@ public class PrincipalPessoa {
     public static void main(String args[]) {
         Pessoa p = new Pessoa("João", 18);
         p.falar();
-    }
 
+          //toString
+        System.out.println(p);
+        //não precisa ser p.toString()
+    }
 }
