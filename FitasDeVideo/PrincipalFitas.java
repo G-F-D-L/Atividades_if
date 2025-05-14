62c1d421c3e338ea03d13b7b80443a1d4a8ade82
Gabriel F <78265172+G-F-D-L@users.noreply.github.com>
2025-05-14 10:25:14 -0300

    Update PrincipalFitas.java

--- FitasDeVideo/PrincipalFitas.java
+++ FitasDeVideo/PrincipalFitas.java
@@ -1,17 +1,17 @@
 public class PrincipalFitas {
     public static void main(String[] args) {
-        // Criação de fitas de vídeo
+        //criação de fitas de vídeo
         FitasDeVideo fita1 = new FitasDeVideo("Star Wars: Guerra dos Clones", 5.0);
         FitasDeVideo fita2 = new FitasDeVideo("MIB: Homens de Preto", 3.5);
         FitasDeVideo fita3 = new FitasDeVideo("Senhor dos anéis: O Retorno do Rei", 7.0);
 
-         // Mostrando os dados das fitas
+         //mostrando os dados das fitas
          System.out.println("Dados das fitas:");
          System.out.println(fita1);
          System.out.println(fita2);
          System.out.println(fita3);
         
-      // Calculando e mostrando o valor do aluguel para diferentes períodos
+      //calculando e mostrando o valor do aluguel para diferentes períodos
       System.out.println("\nValores de aluguel:");
       System.out.println(fita1.getTitulo() + " por 3 dias: R$" + fita1.getValorAluguel(3));
       System.out.println(fita2.getTitulo() + " por 5 dias: R$" + fita2.getValorAluguel(5));
