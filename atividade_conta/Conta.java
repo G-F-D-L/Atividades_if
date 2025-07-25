74c95704a2da299eca5ea7fc4254b92192190a83
G-F-D-L <gabrielfreirelm@gmail.com>
2025-07-25 09:45:45 -0300

    a
    a

---
+++ atividade_conta/Conta.java
@@ -0,0 +1,74 @@
+package atividade_conta;
+
+public class Conta {
+    private int numero;
+    private String titular;
+    private double saldo;
+    private boolean ativa;
+
+    public Conta(int numero, String titular, double saldo, boolean ativa) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+        this.ativa = ativa;
+    }
+
+    public void depositar(double   dinheiro){
+        if (dinheiro > 1 && ativa == true){
+            saldo = saldo + dinheiro;
+        }
+    }
+
+    public void sacar(double   dinheiro){
+        if (dinheiro <= saldo && ativa == true){
+            saldo = saldo - dinheiro;
+        }
+    }
+
+    public void desativar(){
+        if (ativa == true){
+            ativa = false;
+        }
+    }
+
+    public void ativar(){
+        if (ativa == false){
+            ativa = true;
+        }
+    }
+
+    //getters e setters
+    public int getNumero(){
+        return numero;
+    }
+    public String getTitular(){
+        return titular;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    public boolean getAtiva(){
+        return ativa;
+    }
+
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+    public void setAtiva(boolean ativa){
+        this.ativa = ativa;
+    }
+
+    //exibir
+    public String toString(){
+        return "conta de número: " + numero + 
+        "Títular: " + titular +
+        "Saldo: " + saldo +
+        "Ativa?" + ativa ;
+    }
+}
---
+++ atividade_conta/principalConta.java
@@ -0,0 +1,9 @@
+package atividade_conta;
+
+public class principalConta{
+    public static void main(String args[]) {
+        Conta T1 = new Conta(45924873, "Danão", 0, true);
+        Conta T2 = new Conta(8493758, "Tonho", 0, true);
+
+    }
+}
